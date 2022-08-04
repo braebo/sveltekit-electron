f8f124b092999d98a8f638bd71077d35cb6ec755
Eudritch Benjamin <32845411+Eudritch@users.noreply.github.com>
2022-08-04 11:03:19 -0400

    Update adapter setting in svelte.config.js
    fallback option by default is null.  When executing build, "npm run build," fallback is needed.

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
 	kit: {
-		adapter: adapter({}),
+		adapter: adapter({fallback:true}),
 	},
 	preprocess: sveltePreprocess(),
 };
