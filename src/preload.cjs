482d3e98df6b700d2c45a261bc8420dcfc94ac70
sc00 <mail@julian-urabl.com>
2022-09-28 12:18:40 +0200

    Clean up

--- src/preload.cjs
+++ src/preload.cjs
@@ -1,15 +1,15 @@
 // preload.cjs
 
-const { contextBridge, ipcRenderer } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld(
-  'electron', {
-  send: (channel, data) => {
-    ipcRenderer.send(channel, data)
-  },
-  sendSync: (channel, data) => {
-    ipcRenderer.sendSync(channel, data)
-  },
-  receive: (channel, func) => {
-    ipcRenderer.on(channel, (event, ...args) => func(...args))
-  }})
+contextBridge.exposeInMainWorld('electron', {
+	send: (channel, data) => {
+		ipcRenderer.send(channel, data);
+	},
+	sendSync: (channel, data) => {
+		ipcRenderer.sendSync(channel, data);
+	},
+	receive: (channel, func) => {
+		ipcRenderer.on(channel, (event, ...args) => func(...args));
+	},
+});
